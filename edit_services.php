b307e7d3b193e5e4b4a5027621d9c304596ea2f0
Ode Arstiko <dewiharjanto2@gmail.com>
2025-06-05 22:05:31 +0700

    Update edit_services.php

--- edit_services.php
+++ edit_services.php
@@ -4,13 +4,12 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-$servername = "mysql.railway.internal";
-$username = "root";
-$password = "skUAaQacyIAsvrmDjOgBctsJtqPeVzBZ";
-$dbname = "railway";
-$port = 3306;
+$servername = "sql301.infinityfree.com";
+$username = "if0_39158864";
+$password = "nbVTynmrXl90RV";
+$dbname = "if0_39158864_flutter_project_akhir";
 
-$conn = new mysqli($servername, $username, $password, $dbname, $port);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 $id_note = $_POST['id_note'];
 $judul_note = $_POST['judul_note'];
