26e65c143b6325de6748e861abe3ccbae147751c
Ode Arstiko <dewiharjanto2@gmail.com>
2025-06-05 21:50:18 +0700

    Update read_services.php

--- read_services.php
+++ read_services.php
@@ -4,7 +4,14 @@
     header("Access-Control-Allow-Headers: Content-Type, Authorization");
     header("Content-Type: application/json; charset=UTF-8");
 
-    $koneksi = new mysqli('localhost', 'root','','flutter_project_akhir');
+    $servername = "mysql.railway.internal";
+    $username = "root";
+    $password = "skUAaQacyIAsvrmDjOgBctsJtqPeVzBZ";
+    $dbname = "railway";
+    $port = 3306;
+    
+    $conn = new mysqli($servername, $username, $password, $dbname, $port);
+
     $query = mysqli_query($koneksi,'SELECT * FROM note');
     $data = mysqli_fetch_all ($query, MYSQLI_ASSOC);
     echo json_encode($data);
