29f1ce87e3d1a45aea6c4bb7789fd906c97642ca
Ode Arstiko <dewiharjanto2@gmail.com>
2025-06-07 15:15:11 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,1 +1,17 @@
+<?php
+// konfigurasi koneksi database
+$servername = "sql12.freesqldatabase.com";
+$username = "sql12783285";
+$password = "zfzP9skHps";
+$dbname = "sql12783285";
+$port = 3306;
 
+// buat koneksi
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
+
+// cek koneksi
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+// kalau berhasil, $conn siap dipakai di file lain
+?>
