90c558bfb3dae0dcb33172a4e92090c2a8bdaa91
Ode Arstiko <dewiharjanto2@gmail.com>
2025-06-12 19:32:57 +0700

    Update read_services.php

--- read_services.php
+++ read_services.php
@@ -3,10 +3,16 @@
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
     header("Access-Control-Allow-Headers: Content-Type, Authorization");
     header("Content-Type: application/json; charset=UTF-8");
-
     include 'koneksi.php';
 
-    $query = mysqli_query($conn,'SELECT * FROM note');
-    $data = mysqli_fetch_all ($query, MYSQLI_ASSOC);
+    session_start();
+    $email = $_SESSION['email'];
+
+    $stmt = $conn->prepare("SELECT * FROM note WHERE email = ?");
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    $data = $result->fetch_all(MYSQLI_ASSOC);
     echo json_encode($data);
 ?>
