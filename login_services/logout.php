0c416bd57b942ff66511dfeb133911ddb4d6bbb5
Ode Arstiko <dewiharjanto2@gmail.com>
2025-06-12 19:35:10 +0700

    Update logout.php

--- login_services/logout.php
+++ login_services/logout.php
@@ -1,1 +1,11 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Content-Type: application/json; charset=UTF-8");
 
+session_start();
+session_unset();
+session_destroy();
+
+exit();
